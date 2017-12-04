4ceef41492b52a87d64a470b6f5c85c121b37074
Andreas Brommund <andreas@brommund.se>
2017-12-04 12:10:11 +0100

    2 Dec task 2

--- 2dec.py
+++ 2dec.py
@@ -2,21 +2,48 @@ import sys
 
 def main():    
     
-    checksum = 0
-    
-    for line in sys.stdin:
+    matrix = []
         
+    index = 0
+    for line in sys.stdin:
+        matrix.append([])
+        for char in line.split():
+            matrix[index].append(int(char))
+        index += 1
+            
+
+    # Task 1
+    checksum = 0
+    for row in range(0,len(matrix)):
+    
         maximum = float('-inf')
         minimum = float('inf')
 
-        for char in line.split():
-            maximum = max(int(char),maximum)
-            minimum = min(int(char),minimum)
+        for value in matrix[row]:
+            maximum = max(value,maximum)
+            minimum = min(value,minimum)
         
         checksum += maximum - minimum
 
     print(checksum)
-    
+   
+    # Task 2
+    checksum = 0
+    for row in range(0,len(matrix)):
+        next_row = False
+        for index,value1 in enumerate(matrix[row]):
+            for value2 in matrix[row][index+1:]:
+                bigger = max(value1,value2)
+                smaller = min(value1,value2)                   
+                
+                if bigger % smaller == 0:
+                    checksum += bigger // smaller
+                    next_row = True
+                    break
+            if next_row:
+                break
+            
+    print(checksum)
 if __name__ == "__main__":
     main()
 
